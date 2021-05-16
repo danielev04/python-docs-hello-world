c54134d96967ab18d7e41c5d491b7b9c3e7b937d
danielev04 <daniele@vottero.eu>
2021-05-16 19:10:45 +0200

    new target

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    response = requests.get("http://10.0.0.3/companies")
+    print(response.status_code)
+    json = str(response.content)
+    return json
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001)
